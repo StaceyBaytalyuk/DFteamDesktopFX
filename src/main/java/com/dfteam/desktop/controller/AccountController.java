87d20a41c28c8bd45c0df0b51bf39f238187463f
Stacey Baytalyuk <stacey.baytalyuk@gmail.com>
2018-08-27 21:42:49 +0300

    save token

--- src/main/java/com/dfteam/desktop/controller/AccountController.java
+++ src/main/java/com/dfteam/desktop/controller/AccountController.java
@@ -1,4 +1,107 @@
 package com.dfteam.desktop.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AccountController {
+    private String token;
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @FXML
+    private VBox googPanel;
+
+    @FXML
+    private VBox oceanPanel;
+
+    @FXML
+    private VBox amazPanel;
+
+    public AccountController(){
+
+    }
+
+    @FXML
+    private void initialize() {
+        File file2 = new File(System.getProperty("user.home")+File.separator+".dfteam"+File.separator+"config.json");
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader tmp = new FileReader(file2);
+            JSONObject json = (JSONObject) parser.parse(tmp);
+            tmp.close();
+            token = (String) json.get("token");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("http://167.99.138.88:8000/accountlist");
+        get.addHeader("Authorization", token);
+
+        try {
+            HttpResponse response = client.execute(get);
+            JSONObject json = (JSONObject) parser.parse(EntityUtils.toString(response.getEntity()));
+            System.out.println(json.toString());
+            JSONArray arr = (JSONArray)json.get("do");
+            for(int i =0; i<arr.size(); i++){
+                System.out.println(arr.get(i));
+                Button b = new Button((String) arr.get(i));
+                oceanPanel.getChildren().add(b);
+                /*b.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        openVMs(token, "do", b.getText());
+                    }
+                });*/
+            }
+
+            arr = (JSONArray)json.get("ec2");
+            for(int i =0; i<arr.size(); i++){
+                System.out.println(arr.get(i));
+                Button b = new Button((String) arr.get(i));
+                amazPanel.getChildren().add(b);
+                /*b.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        openVMs(token, "ec2", b.getText());
+                    }
+                });*/
+            }
+
+            arr = (JSONArray)json.get("gce");
+            for(int i =0; i<arr.size(); i++){
+                System.out.println(arr.get(i));
+                Button b = new Button((String) arr.get(i));
+                googPanel.getChildren().add(b);
+                /*b.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        openVMs(token, "gce", b.getText());
+                    }
+                });*/
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void openVMs() {
+
+    }
 }
--- src/main/java/com/dfteam/desktop/controller/LoginController.java
+++ src/main/java/com/dfteam/desktop/controller/LoginController.java
@@ -19,7 +19,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import javafx.fxml.FXML;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -37,7 +37,57 @@ public class LoginController {
 
     @FXML
     private void initialize() {
+        if(HomeDir.exists()){
+            if(validToken()){
+                try {
+                    StartAccount();
+                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else{
+            HomeDir.mkdir();
+        }
         btnOK.setOnAction(event -> onOK());
+    }
+
+    private File HomeDir = new File(System.getProperty("user.home")+File.separator+".dfteam");
+
+    private File ConfigFile = new File(HomeDir.getPath()+File.separator+"config.json");
+
+    private boolean validToken(){
+        JSONObject json;
+        JSONParser parser = new JSONParser();
+        if(ConfigFile.exists()){
+            try {
+                FileReader tmp = new FileReader(ConfigFile);
+                json = (JSONObject) parser.parse(tmp);
+                tmp.close();
+                return json.get("token") != null;
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private void AddFile(String token){
+        JSONObject json = new JSONObject();
+        try {
+            ConfigFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        json.put("token", token);
+        try {
+            FileWriter tmp2 = new FileWriter(ConfigFile);
+            tmp2.write(json.toJSONString());
+            tmp2.flush();
+            tmp2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void onOK() {
@@ -55,15 +105,11 @@ public class LoginController {
             JSONParser parser = new JSONParser();
             JSONObject json = (JSONObject) parser.parse(EntityUtils.toString(response.getEntity()));
             if((boolean) json.get("auth") && json.get("error") == null){
-                Stage accountStage = new Stage();
-                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("accounts.fxml")));
-                accountStage.setTitle("DFteam - Accounts");
-                accountStage.getIcons().add(new Image("/images/DF.png"));
-                accountStage.setScene(new Scene(root));
-                accountStage.show();
+                AddFile((String) json.get("token"));
                 System.out.println(json.get("token"));
+                StartAccount();
             }else{
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Auth Error");
                 alert.setHeaderText(null);
                 alert.setContentText("Wrong login or password");
@@ -72,5 +118,15 @@ public class LoginController {
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    private void StartAccount() throws IOException {
+
+        Stage accountStage = new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("accounts.fxml")));
+        accountStage.setTitle("DFteam - Accounts");
+        accountStage.getIcons().add(new Image("/images/DF.png"));
+        accountStage.setScene(new Scene(root));
+        accountStage.show();
     }
 }
