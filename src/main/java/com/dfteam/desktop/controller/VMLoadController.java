4e4c7c963bf228d46cf6cc693e9499ca2418b11f
dimchik <dimak3816@gmail.com>
2018-10-17 00:18:05 +0300

    Update charts

--- src/main/java/com/dfteam/desktop/controller/VMLoadController.java
+++ src/main/java/com/dfteam/desktop/controller/VMLoadController.java
@@ -3,9 +3,13 @@ package com.dfteam.desktop.controller;
 import com.dfteam.desktop.util.VMLoad;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 
@@ -101,8 +105,16 @@ public class VMLoadController {
                         xRAM.setLowerBound(xRAM.getLowerBound() + 1d);
                         xRAM.setUpperBound(xRAM.getUpperBound() + 1d);
                     }
-                    series1.getData().add(new XYChart.Data<Integer, Double>(finalI, cpu));
-                    series2.getData().add(new XYChart.Data<Integer, Double>(finalI, ram));
+                    XYChart.Data<Integer, Double> data = new XYChart.Data<Integer, Double>(finalI, cpu);
+                    data.setNode(
+                            new HoveredThresholdNode(finalI, cpu)
+                    );
+                    XYChart.Data<Integer, Double> data1 = new XYChart.Data<Integer, Double>(finalI, ram);
+                    data1.setNode(
+                            new HoveredThresholdNode(finalI, ram)
+                    );
+                    series1.getData().add(data);
+                    series2.getData().add(data1);
                 });
                 i++;
                 try {
@@ -123,4 +135,40 @@ public class VMLoadController {
         if(vmLoad.isConnected())
             vmLoad.disconnect();
     }
+
+    class HoveredThresholdNode extends StackPane {
+        HoveredThresholdNode(int priorValue, double value) {
+            setPrefSize(10, 10);
+
+            final Label label = createDataThresholdLabel(priorValue, value);
+
+            setOnMouseEntered(mouseEvent -> {
+                getChildren().setAll(label);
+                setCursor(Cursor.NONE);
+                toFront();
+            });
+            setOnMouseExited(mouseEvent -> {
+                getChildren().clear();
+                setCursor(Cursor.CROSSHAIR);
+            });
+        }
+
+        private Label createDataThresholdLabel(int priorValue, double value) {
+            final Label label = new Label(value + "");
+            label.getStyleClass().addAll("default-color0", "chart-line-symbol", "chart-series-line");
+            label.setStyle("-fx-font-size: 13; -fx-font-weight: bold;");
+
+            if (priorValue == 0) {
+                label.setTextFill(Color.DARKGRAY);
+            } else if (value > priorValue) {
+                label.setTextFill(Color.FORESTGREEN);
+            } else {
+                label.setTextFill(Color.FIREBRICK);
+            }
+
+            label.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+            return label;
+        }
+    }
+
 }
